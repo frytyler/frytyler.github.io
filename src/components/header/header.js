d7b813dc79da8256a855416781551bd03d987750
Tyler Fry <fry.tyler@gmail.com>
2018-09-26 07:48:14 -0400

    Adding site title

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,37 @@
 import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+import { Helmet } from 'react-helmet'
 
 import { Hero, Avatar, Tag } from './header.styled'
 
 const Header = () => {
   return (
-    <Hero>
-      <Avatar />
-      <Tag>Hello</Tag>
-    </Hero>
+    <StaticQuery
+      query={graphql`
+        query {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={({
+        site: {
+          siteMetadata: { title },
+        },
+      }) => {
+        return (
+          <Hero>
+            <Helmet>
+              <title>{title}</title>
+            </Helmet>
+            <Avatar />
+            <Tag>Hello</Tag>
+          </Hero>
+        )
+      }}
+    />
   )
 }
 
